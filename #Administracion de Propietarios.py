75b4a3bdaeeed276acc394b75310bcbfcdfc0f72
Dotii <vero.stylinson@gmail.com>
2026-06-12 18:13:59 -0400

    Actualizacion propieta

--- #Administracion de Propietarios.py
+++ #Administracion de Propietarios.py
@@ -1,7 +1,8 @@
 #Administracion de Propietarios
 #Diccionario idependiente, numero departamento y nombre propietario ORGANIZACION
 #REQUISITO consulta de propietarios, actualizacion de informacion, agregar departamentos, eliminar registros, mostrar datos.
-#Diccionarios
+# Diccionarios por piso
+
 piso1 = {
     "101": "Juan Vélez",
     "102": "María Soto"
@@ -11,23 +12,19 @@ piso2 = {
     "201": "Pedro Rojas",
     "202": "Ana Díaz"
 }
+
 piso3 = {
     "301": "Carlos Pérez",
     "302": "Claudia Torres"
 }
+
 piso4 = {
     "401": "José González",
     "402": "Camila Muñoz"
 }
-edificio = {
-    1: piso1,
-    2: piso2,
-    3: piso3,
-    4: piso4
-}
 
 while True:
-    print(" ADMINISTRACIÓN DEL EDIFICIO ")
+    print("\n--- ADMINISTRACIÓN DE PROPIETARIOS ---")
     print("1. Consultar propietario")
     print("2. Agregar departamento")
     print("3. Modificar propietario")
@@ -38,23 +35,73 @@ while True:
     opcion = input("Seleccione una opción: ")
 
     if opcion == "1":
-        piso = int(input("Ingrese el piso (1-4): "))
-        depto = input("Ingrese el número de departamento: ")
+        piso = input("Ingrese el piso (1-4): ")
+        depto = input("Ingrese número de departamento: ")
+        if piso == "1":
+            print("Propietario:", piso1.get(depto, "No existe"))
+        elif piso == "2":
+            print("Propietario:", piso2.get(depto, "No existe"))
+        elif piso == "3":
+            print("Propietario:", piso3.get(depto, "No existe"))
+        elif piso == "4":
+            print("Propietario:", piso4.get(depto, "No existe"))
 
-        if depto in edificio[piso]:
-            print("Propietario:", edificio[piso][depto])
+    elif opcion == "2":
+        piso = input("Ingrese el piso (1-4): ")
+        depto = input("Número de departamento: ")
+        nombre = input("Nombre del propietario: ")
+
+        if piso == "1":
+            piso1[depto] = nombre
+        elif piso == "2":
+            piso2[depto] = nombre
+        elif piso == "3":
+            piso3[depto] = nombre
+        elif piso == "4":
+            piso4[depto] = nombre
+
+        print("Departamento agregado.")
+
+    elif opcion == "3":
+        piso = input("Ingrese el piso (1-4): ")
+        depto = input("Número de departamento: ")
+        nuevo_nombre = input("Nuevo propietario: ")
+
+        if piso == "1" and depto in piso1:
+            piso1[depto] = nuevo_nombre
+        elif piso == "2" and depto in piso2:
+            piso2[depto] = nuevo_nombre
+        elif piso == "3" and depto in piso3:
+            piso3[depto] = nuevo_nombre
+        elif piso == "4" and depto in piso4:
+            piso4[depto] = nuevo_nombre
         else:
             print("Departamento no encontrado.")
 
-    elif opcion == "2":
-        piso = int(input("Ingrese el piso (1-4): "))
+    elif opcion == "4":
+        piso = input("Ingrese el piso (1-4): ")
         depto = input("Número de departamento: ")
-        propietario = input("Nombre del propietario: ")
 
-        edificio[piso][depto] = propietario
-        print("Departamento agregado correctamente.")
-    
-    if opcion == "3":
-        propietario =  (input("Igrese propietario a editar"))
+        if piso == "1" and depto in piso1:
+            del piso1[depto]
+        elif piso == "2" and depto in piso2:
+            del piso2[depto]
+        elif piso == "3" and depto in piso3:
+            del piso3[depto]
+        elif piso == "4" and depto in piso4:
+            del piso4[depto]
+        else:
+            print("Departamento no encontrado.")
 
-   Woa
+    elif opcion == "5":
+        print("\nPISO 1:", piso1)
+        print("PISO 2:", piso2)
+        print("PISO 3:", piso3)
+        print("PISO 4:", piso4)
+
+    elif opcion == "6":
+        print("Programa finalizado.")
+        break
+
+    else:
+        print("Opción no válida.")
